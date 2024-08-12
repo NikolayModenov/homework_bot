ab14ed2b2f3366476d10cd3e390bc239f6c6fe3f
Николай Моденов <uptb.modenov@yandex.ru>
2024-08-12 20:30:14 +0300

    "edit README"

--- my_handler.py
+++
@@ -1,9 +0,0 @@
-import logging
-from logging.handlers import RotatingFileHandler
-
-
-class MyLogsHandler(RotatingFileHandler):
-
-    # def emit(self, record):
-    #     log_entry = self.format(record)
-    #     # тут ваша логика
